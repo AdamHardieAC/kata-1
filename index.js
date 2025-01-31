8b66ba8c0da51ba7bcecd6b9b09e361e3302c771
AdamHardieAC <adam.hardie@arnoldclark.com>
2025-01-31 12:26:04 +0000

    Urnfield to Int method + test suite

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ function IntToUrnfield(int) {
   const fiveCount = Math.floor(int / 5);
   return '/'.repeat(oneCount) + '~'.repeat(fiveCount);
 }
-module.exports = { IntToUrnfield };
+
+function UrnfieldToInt(urnfield) {
+  if (urnfield === '') return 0;
+  return [...urnfield].reduce((count, char) =>
+    count + (char === '/' ? 1 : 5), 0);
+}
+
+module.exports = { IntToUrnfield, UrnfieldToInt };
---
+++ spec/urnfieldtoint.test.js
@@ -0,0 +1,37 @@
+const { UrnfieldToInt } = require('../index');
+
+test('Urnfield empty string equals 0', () => {
+  expect(UrnfieldToInt('')).toBe(0);
+});
+
+test('Urnfield / string equals 1', () => {
+  expect(UrnfieldToInt('/')).toBe(1);
+});
+
+test('Urnfield ~ string equals 5', () => {
+  expect(UrnfieldToInt('~')).toBe(5);
+});
+
+test('Urnfield // string equals 2', () => {
+  expect(UrnfieldToInt('//')).toBe(2);
+});
+
+test('Urnfield ~~ string equals 10', () => {
+  expect(UrnfieldToInt('~~')).toBe(10);
+});
+
+test('Urnfield /~ string equals 6', () => {
+  expect(UrnfieldToInt('/~')).toBe(6);
+});
+
+test('Urnfield //~ string equals 7', () => {
+  expect(UrnfieldToInt('//~')).toBe(7);
+});
+
+test('Urnfield /~~ string equals 11', () => {
+  expect(UrnfieldToInt('/~~')).toBe(11);
+});
+
+test('Urnfield //~~ string equals 12', () => {
+  expect(UrnfieldToInt('//~~')).toBe(12);
+});
