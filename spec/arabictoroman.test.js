9634da277ed1c9f18f375fe2a58eb01ad7f6dc92
AdamHardieAC <adam.hardie@arnoldclark.com>
2025-06-20 12:25:23 +0100

    Arabic to roman WIP

--- spec/arabictoroman.test.js
+++ spec/arabictoroman.test.js
@@ -1,51 +1,195 @@
 const ArabicToRoman = require('../src/modules/ArabicToRoman');
-
 const testCases = [
-  // {
-  //   symbol: 'I',
-  //   value: 1
-  // },
   {
-    symbol: 'V',
+    symbol: "I",
+    value: 1
+  },
+  {
+    symbol: "V",
     value: 5
   },
   {
-    symbol: 'X',
+    symbol: "X",
     value: 10
   },
   {
-    symbol: 'L',
+    symbol: "L",
     value: 50
   },
   {
-    symbol: 'C',
+    symbol: "C",
     value: 100
   },
   {
-    symbol: 'D',
+    symbol: "D",
     value: 500
   },
   {
-    symbol: 'M',
+    symbol: "M",
     value: 1000
   },
   {
-    symbol: 'II',
+    symbol: "III",
+    value: 3
+  },
+  {
+    symbol: "II",
     value: 2
   },
   {
-    symbol: 'CCV',
-    value: 205
-  },
-  {
-    symbol: 'IV',
+    symbol: "IV",
     value: 4
   },
+  {
+    symbol: "VI",
+    value: 6
+  },
+  {
+    symbol: "VII",
+    value: 7
+  },
+  {
+    symbol: "IX",
+    value: 9
+  },
+  {
+    symbol: "MDCLXVI",
+    value: 1666
+  },
+  {
+    symbol: "DCLXVI",
+    value: 666
+  },
+  {
+    symbol: "CLXVI",
+    value: 166
+  },
+  {
+    symbol: "LXVI",
+    value: 66
+  },
+  {
+    symbol: "XVI",
+    value: 16
+  },
+  {
+    symbol: "XIV",
+    value: 14
+  },
+  {
+    symbol: "XIX",
+    value: 19
+  },
+  {
+    symbol: "XL",
+    value: 40
+  },
+  {
+    symbol: "LX",
+    value: 60
+  },
+  {
+    symbol: "XC",
+    value: 90
+  },
+  {
+    symbol: "CC",
+    value: 200
+  },
+  {
+    symbol: "CD",
+    value: 400
+  },
+  {
+    symbol: "D",
+    value: 500
+  },
+  {
+    symbol: "DC",
+    value: 600
+  },
+  {
+    symbol: "DCC",
+    value: 700
+  },
+  {
+    symbol: "CM",
+    value: 900
+  },
+  {
+    symbol: "IM",
+    value: 999
+  },
+  {
+    symbol: "MI",
+    value: 1001
+  },
+  {
+    symbol: "MCM",
+    value: 1900
+  },
+  {
+    symbol: "MCLXIV",
+    value: 1164
+  },
+  {
+    symbol: "CCI",
+    value: 201
+  },
+  {
+    symbol: "MDCCLXIV",
+    value: 1764
+  },
+
 ];
+// const testCases = [
+//   // {
+//   //   symbol: 'I',
+//   //   value: 1
+//   // },
+//   {
+//     symbol: 'V',
+//     value: 5
+//   },
+//   {
+//     symbol: 'X',
+//     value: 10
+//   },
+//   {
+//     symbol: 'L',
+//     value: 50
+//   },
+//   {
+//     symbol: 'C',
+//     value: 100
+//   },
+//   {
+//     symbol: 'D',
+//     value: 500
+//   },
+//   {
+//     symbol: 'M',
+//     value: 1000
+//   },
+//   {
+//     symbol: 'II',
+//     value: 2
+//   },
+//   {
+//     symbol: 'CCV',
+//     value: 205
+//   },
+//   {
+//     symbol: 'IV',
+//     value: 4
+//   },
+// ];
 test('base case 0 = nulla', () => {
   expect(ArabicToRoman(0)).toBe('nulla');
 });
-
+test('9 to equal IX', () => {
+  expect(ArabicToRoman(9)).toBe('IX');
+});
 
 testCases.forEach((scenario) => {
   test(`${scenario.value} to equal ${scenario.symbol}`, () => {
--- src/modules/ArabicToRoman.js
+++ src/modules/ArabicToRoman.js
@@ -10,11 +10,20 @@ function ArabicToRoman(num) {
     'M': 1000
   };
   
-  
-  return Object.entries(romanNumerals).reverse().reduce((accumulator, [roman, value]) => {
+  return Object.entries(romanNumerals).reverse().reduce((accumulator, [roman, value], currentIndex) => {
+    // const lastDigit = parseInt(num.toString().split('').reverse()[0]);
+    const lastDigit = parseInt(accumulator.num.toString().split('').reverse()[0]);
+    //[['i', 1], ['v', 5]]
+
     const count = Math.floor(accumulator.num/value);
-    accumulator.result += roman.repeat(count);
-    accumulator.num -= value * count;
+    if (lastDigit === 4  && roman === 'V' || lastDigit === 9 && roman === 'X') {
+      console.log('subtract');
+      accumulator.result += `I${roman}`;
+      accumulator.num -= value * count;
+    } else {
+      accumulator.result += roman.repeat(count);
+      accumulator.num -= value * count;
+    }
     return accumulator;
   }, {num, result: ''}).result;
 }
