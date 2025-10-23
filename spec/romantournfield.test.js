d99021edb73996c3c3ee180a1eab6b602f1a635a
AdamHardieAC <adam.hardie@arnoldclark.com>
2025-10-23 10:58:11 +0100

    RomanToUrnField

--- spec/romantournfield.test.js
+++ spec/romantournfield.test.js
@@ -9,6 +9,10 @@ test('Roman I equals /', () => {
   expect(RomanToUrnfield('I')).toBe('/');
 });
 
+test('Roman II equals //', () => {
+  expect(RomanToUrnfield('II')).toBe('//');
+});
+
 test('Roman V equals ~', () => {
   expect(RomanToUrnfield('V')).toBe('~');
 });
@@ -39,6 +43,50 @@ test('Roman XI equals /~~', () => {
 });
 
 
-test.only('Roman IV equals ////', () => {
+test('Roman IV equals ////', () => {
   expect(RomanToUrnfield('IV')).toBe('////');
+});
+
+test('Roman XL equals ~~~~~~~~', () => {
+  expect(RomanToUrnfield('XL')).toBe('~'.repeat(8));
+});
+
+test('Roman CM equals ~~~~~~~~', () => {
+  expect(RomanToUrnfield('CM')).toBe('~'.repeat(180));
+});
+
+test('Roman XC equals ~~~~~~~~', () => {
+  expect(RomanToUrnfield('XC')).toBe('~'.repeat(18));
+});
+
+test('Roman VII equals //~', () => {
+  expect(RomanToUrnfield('VII')).toBe('/'.repeat(2) + '~'.repeat(1));
+});
+
+test('Roman XIV equals ////~~', () => {
+  expect(RomanToUrnfield('XIV')).toBe('/'.repeat(4) + '~'.repeat(2));
+});
+
+test('Roman XIX equals /////~~~', () => {
+  expect(RomanToUrnfield('XIX')).toBe('/'.repeat(4) + '~'.repeat(3));
+});
+
+test('Roman XIII equals ///~~', () => {
+  expect(RomanToUrnfield('XIII')).toBe('/'.repeat(3) + '~'.repeat(2));
+});
+
+test('Roman XCIX equals ///~~', () => {
+  expect(RomanToUrnfield('XCIX')).toBe('/'.repeat(4) + '~'.repeat(19));
+});
+
+test('Roman XXIV equals ////~~~~', () => {
+  expect(RomanToUrnfield('XXIV')).toBe('/'.repeat(4) + '~'.repeat(4));
+});
+
+test('Roman CXCIX equals ///~~', () => {
+  expect(RomanToUrnfield('CXCIX')).toBe('/'.repeat(4) + '~'.repeat(39));
+});
+
+test('Roman MMMCMXCIX equals ~~~~~~~~', () => {
+  expect(RomanToUrnfield('MMMCMXCIX')).toBe('/'.repeat(4) + '~'.repeat(799));
 });
--- src/modules/RomanToUrnfield.js
+++ src/modules/RomanToUrnfield.js
@@ -1,28 +1,24 @@
 function RomanToUrnfield(roman) {
   if (roman === 'nulla') return '';
 
-  // const romanMap = {
-  //   I: '/', V: '~', X: '~~', L: '~'.repeat(10), 
-  //   C: '~'.repeat(20), D: '~'.repeat(100), M: '~'.repeat(200)
-  // }
   const romanMap = {
     I: 1, V: 5, X: 10, L: 50, 
     C: 100, D: 500, M: 1000
   }
-  //IV
-  ///testertste
-  // current = I | /, next = V | ~
+
   let returnNum = 0;
   for(let c = 0; c < roman.length; c++) {
     const currentNumber = romanMap[roman[c]];
     const nextNumber = romanMap[roman[c+1]] || 0;
-    if (!nextNumber) break;
     if (nextNumber > currentNumber) {
-      returnNum = nextNumber - currentNumber;
+      returnNum += nextNumber - currentNumber;
+      c++;
+      console.log({where: 'greater', roman, returnNum, currentNumber, nextNumber});
     } else {
-      returnNum = returnNum + currentNumber;
+      returnNum += currentNumber;
+      console.log({where: 'else', roman, returnNum, currentNumber, nextNumber});
     }
-    console.log({roman, returnNum, currentNumber, nextNumber});
+    console.log({where: 'outer', roman, returnNum, currentNumber, nextNumber});
   }
   const fiveCount = returnNum/5;
   const oneCount = returnNum % 5;
